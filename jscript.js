e1fc12a54fc922375b7c6bd6fa8e1b883e0aa68b
PedroLDN <pedrofrankstein13@hotmail.com>
2015-08-25 09:50:59 -0300

    Término de uma das páginas de amostras para meu portfólio - WRD
    WRD - Wonderfull Responsive Design

---
+++ jscript.js
@@ -0,0 +1,45 @@
+﻿(function(){
+	
+	var addEvent = function(elem, type, callback){
+	    
+		if(elem.addEventListener){
+		    addEvent = function(elem, type, callback){
+				elem.addEventListener(type, callback, false);
+			}
+		}
+		else{
+		    addEvent = function(elem, type, callback){
+				elem.attachEvent(type, callback);
+			}
+		}
+		
+		addEvent(elem, type, callback);
+		
+	}
+	
+	var doc = document;
+	
+	var menuButton = doc.getElementById("menu");
+	var menu = doc.getElementById("h-nav");
+	var menuLinks = menu.getElementsByTagName("a");
+	menu.className = "hidden";	
+	
+	function closeMenu(){
+	    menu.className = "hidden";
+	}
+	
+	var len = menuLinks.length;
+	for(var a=0; a < len; a++){
+	    addEvent(menuLinks[a], "click", closeMenu);
+	}
+	
+	addEvent(menuButton, "click", function(e){
+		if(menu.className === "hidden"){
+			menu.className = "show";
+		}
+		else{
+			menu.className = "hidden";
+		}
+	})
+	
+}());
